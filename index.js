3c24233f7b696b5dfdb171c7e44a3c7ee84f2618
masud-1971 <msd.rana.2008@gmail.com>
2025-01-20 23:08:46 +0600

    solved problem #2
    solved problem #2

--- index.js
+++ index.js
@@ -16,40 +16,40 @@
 // Recursive function to generate
 // all permutations of strings
 
-function recurPermute(index, s, ans) {
-
-    // Base Case
-    if (index === s.length) {
-        ans.add(s.join(""));
-        return;
-    }
-
-    // Swap the current index with all
-    // possible indices and recur
-    for (let i = index; i < s.length; i++) {
-        [s[index], s[i]] = [s[i], s[index]];
-        recurPermute(index + 1, s, ans);
-        [s[index], s[i]] = [s[i], s[index]];
-    }
-}
-
-// Function to find all unique permutations
-function findPermutation(s) {
-
-    // sort input string
-    s = s.split("").sort();
-
-    // Stores all unique permutations
-    let res = new Set();
-    recurPermute(0, s, res);
-
-    // Convert Set to Array for the final answer
-    return Array.from(res).sort();
-}
-
-const s = "ABC";
-const res = findPermutation(s);
-console.log(res.join(" "));
+// function recurPermute(index, s, ans) {
+//
+//     // Base Case
+//     if (index === s.length) {
+//         ans.add(s.join(""));
+//         return;
+//     }
+//
+//     // Swap the current index with all
+//     // possible indices and recur
+//     for (let i = index; i < s.length; i++) {
+//         [s[index], s[i]] = [s[i], s[index]];
+//         recurPermute(index + 1, s, ans);
+//         [s[index], s[i]] = [s[i], s[index]];
+//     }
+// }
+//
+// // Function to find all unique permutations
+// function findPermutation(s) {
+//
+//     // sort input string
+//     s = s.split("").sort();
+//
+//     // Stores all unique permutations
+//     let res = new Set();
+//     recurPermute(0, s, res);
+//
+//     // Convert Set to Array for the final answer
+//     return Array.from(res).sort();
+// }
+//
+// const s = "ABC";
+// const res = findPermutation(s);
+// console.log(res.join(" "));
 
 
 
@@ -84,3 +84,33 @@ console.log(res.join(" "));
 // // কার্যকরী কাজ করা এবং ফলাফল দেখা
 // const result = generatePermutations(inputString);
 // console.log(result);
+
+
+
+
+
+// *********************************
+// Write a program to find the length of the longest substring in a given string without repeating characters. For example, in the string "abcabcbb," the longest substring without repeating characters is "abc," which has a length of 3.
+//******************
+
+function lengthOfLongestSubstring(s) {
+    let maxLength = 0;
+    let start = 0;
+    const charMap = {};
+
+    for (let end = 0; end < s.length; end++) {
+        if (charMap[s[end]] !== undefined) {
+            start = Math.max(charMap[s[end]] + 1, start);
+        }
+        charMap[s[end]] = end;
+        maxLength = Math.max(maxLength, end - start + 1);
+    }
+
+    return maxLength;
+}
+
+// উদাহরণ ইনপুট
+const inputString = "abcabcbb";
+// সর্বোচ্চ সাবস্ট্রিংয়ের দৈর্ঘ্য বের করা
+const result = lengthOfLongestSubstring(inputString);
+console.log(result); // আউটপুট হবে 3
